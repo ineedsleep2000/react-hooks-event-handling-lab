a9ed6445eca604ba025735a82b7bb1e82bb5cf21
Brian <turtle40965@gmail.com>
2024-05-15 17:34:09 -0400

    complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+  function handleOnFocus(event) {
+    console.log("Good!");
+  }
+  function handleOnBlur(event) {
+    console.log("Hey! Eyes on me!");
+  }
+
+  return (
+    <button onFocus={handleOnFocus} onBlur={handleOnBlur}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,16 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function handleChange(event) {
+    //console.log(event.target.value);
+    console.log("Entering password...");
+  }
+
+  return (
+    <div>
+      <input type="password" onChange={handleChange} />
+    </div>
+  );
 }
 
 export default Keypad;
